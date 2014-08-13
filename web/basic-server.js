8273db2b4a920fb364b623a73415b536bf5a86f9
stefceror <stefsnow@gmail.com>
2014-08-13 15:51:07 -0700

    Basic routeMap built

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,9 +1,22 @@
 var http = require("http");
 var handler = require("./request-handler");
+var url = require('url');
 
 var port = 8080;
 var ip = "127.0.0.1";
-var server = http.createServer(handler.handleRequest);
+
+var routeMap = {
+  '/': handler.handleRequest
+}
+
+var server = http.createServer(function(request,reponse){
+  var parsedUrl = url.parse(request.url);
+  var path = parsedUrl.pathname;
+  if(routeMap[path]){
+    routeMap[path](request,response);
+  }
+
+});
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
 
